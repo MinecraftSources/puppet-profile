84c02c048e9d28c25a3e85ed60915be2a9f92deb
Jodie Belgrave <rmb1993@gmail.com>
2015-07-22 19:37:03 -0400

    forgot comma

--- manifests/base.pp
+++ manifests/base.pp
@@ -43,7 +43,7 @@ class profile::base {
     action => 'accept',
   }
   firewall {"004 accept all from private network":
-    source => '172.16.0.0/24'
+    source => '172.16.0.0/24',
     proto  => "all",
     state  => ['NEW'],
     action => "accept",
