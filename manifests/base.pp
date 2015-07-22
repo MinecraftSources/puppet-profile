8d18d03e003ed97adeebd581b3b68122c7c738f9
Jodie Belgrave <rmb1993@gmail.com>
2015-07-22 18:58:22 -0400

    Update base.pp

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,18 +1,5 @@
-class profile::base (
-  $search,
-  $nameservers
-) {
+class profile::base {
 
-  validate_string($search)
-  validate_array($nameservers)
-
-  # Manage resolve.conf
-  file { '/etc/resolv.conf':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0644',
-    content => template('profile/resolv.conf.erb')
-  }
+  include ::dns
 
 }
