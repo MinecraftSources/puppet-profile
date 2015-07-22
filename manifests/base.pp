a0be579f704d0d96b9e9f6ecd284108d1d7017cb
Jodie Belgrave <rmb1993@gmail.com>
2015-07-22 19:26:47 -0400

    if el7 remove firewalld

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,5 +1,18 @@
 class profile::base {
 
+  # Manage DNS
   include ::dns
+  
+  case $operatingsystem {
+    'RedHat', 'CentOS': {
+      if $operatingsystemrelease =- /7.*^/ {
+        # Remove firewalld
+        package { 'firewalld':
+          ensure => 'removed'
+        }
+      }
+    }
+  }
+  
 
 }
